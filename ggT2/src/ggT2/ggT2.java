96acbb983ae8b247bb745ecdfd9e36ca10ebb264
Maywop <93273627+Maywop@users.noreply.github.com>
2021-11-02 22:35:56 +0100

    Add files via upload

--- ggT2/src/ggT2/ggT2.java
+++ ggT2/src/ggT2/ggT2.java
@@ -1,10 +1,26 @@
 package ggT2;
 
-public class ggT2 {
+import java.util.Scanner;
+
+public class ggT {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner =new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println("ggT ist:" + ggt(a,b));
+		
 	}
 
+	static int ggt(int a, int b) {
+		if (a == 0 || b == 0)
+			return 0;
+		if (a==b)
+			return a;
+		if(a > b)
+			return ggt(a-b,b);
+		else
+			return ggt(a,b-a);
+	}
 }
